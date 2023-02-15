fa202293915b23cb8e521355509d705c58fc9963
Nick <nick.buscemi13@gmail.com>
2023-02-15 16:12:38 -0500

    Phase3-complete

--- phase-3.js
+++ phase-3.js
@@ -1,19 +1,51 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
-
-  // Your code here
-
+  let data = [];
+  let results = [];
+  let base = increment
+  for (let i = 0; i < 10; i++) {
+    //start clock
+    let startTime = Date.now();
+    
+    results.push(addNums(increment));
+    
+    //end clock
+    let endTime = Date.now();
+    //prints runtime
+    let key = `addNums(${increment}): ${endTime - startTime}ms`
+    //data.push(`Runtime - addNums(${increment}): ${endTime - startTime}ms`)
+    data.push({
+      Runtime: key,
+    })
+    increment += base
+    
+  }
+  //return results;
+  console.log(data)
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+  let data = [];
+  let results = [];
+    let base = increment
+    for (let i = 0; i < 10; i++) {
+      let startTime = Date.now();
+      results.push(addManyNums(increment));
+      let endTime = Date.now();
 
-  // Your code here
+      let key = `addManyNums(${increment}): ${endTime - startTime}ms`
+    //data.push(`Runtime - addNums(${increment}): ${endTime - startTime}ms`)
+    data.push({
+      Runtime: key,
+    })
+      
+      increment += base
+    }
+    //return results;
+    console.log(data)
+  
 
 }
 

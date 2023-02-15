420c8d7f8086fc4cd64a2ba0d62b1094d6d95736
Nick <nick.buscemi13@gmail.com>
2023-02-15 10:17:14 -0500

    Phase2-complete

--- phase-2.js
+++ phase-2.js
@@ -3,12 +3,25 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let results = [];
+  let base = increment
+  for (let i = 0; i < 10; i++) {
+    results.push(addNums(increment));
+    increment += base
+  }
+  return results;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  let results = [];
+  let base = increment
+  for (let i = 0; i < 10; i++) {
+    results.push(addManyNums(increment));
+    increment += base
+  }
+  return results;
 
 }
 
